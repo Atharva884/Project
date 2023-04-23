0f48bfae74e17a9172b7a733f1ae1dfa3bbc13e2
LolzZz <atharvalolzzz96@gmail.com>
2023-04-23 12:32:57 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("Hello");
+
+let formation = document.querySelector('#formation')
+let muds = document.querySelector('#muds')
+let statement = document.querySelector('.statement')
+let mudValue;
+let formationValue;
+
+formation.addEventListener('change', ()=>{
+    formationValue = formation.value;
+    if(formation.value == ""){
+        alert("Select your formation")
+        muds.style.pointerEvents = "none"
+    }
+    console.log(formation.value);
+    muds.disabled = false;
+    muds.style.pointerEvents = "auto"
+})
+
+muds.addEventListener('change', ()=>{
+    mudValue = muds.value;
+    console.log(muds.value);
+
+    console.log("values");
+    console.log(formationValue);
+    console.log(mudValue);
+    if(muds.value == ""){
+        alert("Select your Mud")
+        // muds.disabled = true;
+    }else if(mudValue == "waterBasedMud" && formationValue == "shale"){
+        statement.innerHTML = `
+            <div class="alert alert-info" role="alert">
+                Problem that may arises: <strong>Swelling</strong> 
+            </div>
+        `;
+    }
+})
